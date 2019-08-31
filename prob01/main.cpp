a9b19895a1ea25eee3e4c1797a4f6110152247a4
jmurkijanian <jmurkijanian@gmail.com>
2019-08-31 13:36:05 -0700

    used std, cout, and \n to display diamond shape on terminal

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,19 @@
-// Name: 
+// Name:
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
 int main()
 {
+//using std and cout to display the diamond shape
+std::cout <<"   *\n";
+std::cout <<"  ***\n";
+std::cout <<" *****\n";
+std::cout <<"*******\n";
+std::cout <<" *****\n";
+std::cout <<"  ***\n";
+std::cout <<"   *\n";
 
-  return 0;
+
+return 0;
 }
